ae9aaba5e527c2a527f8fdac22404ec3b10f14ea
motong <femotong@gmail.com>
2021-07-03 18:28:33 +0800

    setup用法实例

---
+++ src/components/ScriptSetup.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <span>{{ num }}</span>
+        <br />
+        <button @click="add">+1</button>
+        <button @click="subtract">-1</button>
+    </div>
+</template>
+
+
+<script setup>
+  import {ref} from 'vue'
+  const num = ref(1)
+  const add = ()=> num.value++;
+  const subtract = ()=> num.value--;
+</script>
+
+
+<!--
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    let num = ref(1)
+    const add = ()=> num.value++;
+    const subtract = ()=> num.value--;
+
+    return {
+      num,
+      add
+    }
+  },
+}
+</script>
+-->
