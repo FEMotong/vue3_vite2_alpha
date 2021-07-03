617dd685944d1e3b80da47e241666eaf4a7a8bbf
motong <femotong@gmail.com>
2021-07-03 19:31:19 +0800

    toRef、toRefs用法实例

--- src/components/MyInstance.vue
+++ src/components/MyInstance.vue
@@ -7,40 +7,54 @@
         <br />
         <span>{{state.count}}</span>
         <br />
-        <button @click="changeCount">改变count</button>
+        <span>state.num：{{state.count}}-num.value{{state.num}}</span>
+        <br />
+        <button @click="changeCount">改变state.count</button>
+        <button @click="changeNum">改变state.num</button>
+        <br />  
+        <p>toRefs：</p>
+        <span>num：{{ num }}-count：{{count}}</span>
     </div>
 </template>
 
-
 <script setup="props">
-  import {ref, reactive} from 'vue'
-  const num = ref(1) // 数字、字符串等简单数据类型响应
+  import {ref, reactive, toRefs} from 'vue'
+  const num = ref(1) // num.value -> 1; 数字、字符串等简单数据类型响应, 响应式包装对象
   const state = reactive({  // 返回对象的响应式副本
-    count: 2
+    count: 2,
+    num,
   })
   const add = ()=> num.value++;
   const subtract = ()=> num.value--;
   const changeCount = ()=> state.count++;
-
+  const changeNum = ()=> state.num++; // reactive 将解包所有深层的 refs，同时维持 ref 的响应性。
 </script>
-
-
 
 <!--
 <script>
-import { ref } from 'vue'
+import { ref, reactive, toRefs } from 'vue'
 
 export default {
   setup(props) {
     // debugger;
     let num = ref(1)
+    const state = reactive({  // 返回对象的响应式副本
+      count: 2,
+      num,
+    })
     const add = ()=> num.value++;
     const subtract = ()=> num.value--;
+    const changeCount = ()=> state.count++;
+    const changeNum = ()=> state.num++; // reactive 将解包所有深层的 refs，同时维持 ref 的响应性。
 
     return {
       num,
       add,
-      subtract
+      state,
+      ...toRefs(state),
+      subtract,
+      changeCount,
+      changeNum
     }
   },
 }
